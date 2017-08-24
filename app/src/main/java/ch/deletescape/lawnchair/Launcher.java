292bb30fa3a8870a02c4fea8cba85a025c2eeae6
iainfm <ifmclaren@gmail.com>
2017-08-24 15:26:18 +0100

    Update Launcher.java
    Added forced Immersive Mode.

--- app/src/main/java/ch/deletescape/lawnchair/Launcher.java
+++ app/src/main/java/ch/deletescape/lawnchair/Launcher.java
@@ -995,6 +995,16 @@ public class Launcher extends Activity
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         mHasFocus = hasFocus;
+        if (mHasFocus) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 
     private boolean acceptFilter() {
